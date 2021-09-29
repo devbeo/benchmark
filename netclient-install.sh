da9943ec4ab458811405901d53743b763afed69b
Anh <54889619+devbeo@users.noreply.github.com>
2021-09-29 23:49:29 +0700

    Update netclient-install.sh

--- netclient-install.sh
+++ netclient-install.sh
@@ -10,6 +10,6 @@ fi
 
 wget -O netclient https://github.com/gravitl/netmaker/releases/download/latest/netclient
 chmod +x netclient
-sudo ./netclient join --name $HOSTNAME -t $KEY
+sudo ./netclient join --name "$HOSTNAME" -t $KEY
 
 rm -f netclient
